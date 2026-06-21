07642e39d960704cbcb900bad800c0548f48c92c
VT-Labs <273777472+vt-labs-admin@users.noreply.github.com>
2026-06-21 10:12:49 +0800

    chore: bump version to 1.0.7 and remove temporary test files

--- fetch_bili.js
+++
@@ -1,4 +0,0 @@
-fetch('https://www.bilibili.com/video/BV1Yp421o7NB/').then(r => r.text()).then(t => {
-    const match = t.match(/<meta[^>]*property="og:image"[^>]*content="([^"]+)"/i);
-    console.log(match ? match[1] : 'No og:image');
-});
